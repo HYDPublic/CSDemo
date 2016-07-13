65c29895931832b46c37117ae9ec45f16727d457
jaearle <jaearle@JAEARLE-WORK>
2016-07-13 15:54:50 -0700

    init Visual Studio commit

---
+++ CSDemo/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CSDemo.Startup))]
+namespace CSDemo
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
