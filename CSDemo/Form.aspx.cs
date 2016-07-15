a207492368dd2c0b86df0b60a0c3b9da10243f97
JamesEarle <j_earle@hotmail.com>
2016-07-14 17:26:07 -0700

    sample change for user auth

--- CSDemo/Form.aspx.cs
+++ CSDemo/Form.aspx.cs
@@ -15,7 +15,7 @@ public partial class Default2 : System.Web.UI.Page
 
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        //sample change
     }
 
     static async void MakeRequest()
